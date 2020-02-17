486761aaa58d53e140119bee968bea3baa6779a6
juloberno <juloberno@gmail.com>
2020-02-17 12:43:54 +0100

    pickling functions for uct behaviors

--- python/python_planner_uct.cpp
+++ python/python_planner_uct.cpp
@@ -4,6 +4,7 @@
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
 #include "python/python_planner_uct.hpp"
+#include "python/polymorphic_conversion.hpp"
 #include <memory>
 #include "src/behavior_uct_single_agent.hpp"
 #include "src/behavior_uct_single_agent_macro_actions.hpp"
@@ -23,12 +24,33 @@ void python_planner_uct(py::module m) {
       .def(py::init<const modules::commons::ParamsPtr &>())
       .def("__repr__", [](const BehaviorUCTSingleAgent &m) {
         return "bark.behavior.BehaviorUCTSingleAgent";
-      });
+      })
+          .def(py::pickle(
+      [](const BehaviorUCTSingleAgent& b) {
+        return py::make_tuple(ParamsToPython(b.GetParams()));
+      },
+      [](py::tuple t) {
+        if (t.size() != 1)
+          throw std::runtime_error("Invalid behavior model state!");
+        /* Create a new C++ instance */
+        return new BehaviorUCTSingleAgent(PythonToParams(t[0].cast<py::tuple>()));
+      }));
+  
   py::class_<BehaviorUCTSingleAgentMacroActions, BehaviorModel,
              std::shared_ptr<BehaviorUCTSingleAgentMacroActions>>(
       m, "BehaviorUCTSingleAgentMacroActions")
       .def(py::init<const modules::commons::ParamsPtr &>())
       .def("__repr__", [](const BehaviorUCTSingleAgentMacroActions &m) {
         return "bark.behavior.BehaviorUCTSingleAgentMacroActions";
-      });
+      })
+          .def(py::pickle(
+      [](const BehaviorUCTSingleAgentMacroActions& b) {
+        return py::make_tuple(ParamsToPython(b.GetParams()));
+      },
+      [](py::tuple t) {
+        if (t.size() != 1)
+          throw std::runtime_error("Invalid behavior model state!");
+        /* Create a new C++ instance */
+        return new BehaviorUCTSingleAgentMacroActions(PythonToParams(t[0].cast<py::tuple>()));
+      }));
 }
