b298dfb0733826927ec974bbe4eb9cb127694c27
juloberno <juloberno@gmail.com>
2020-07-28 15:51:33 +0200

    1D complex test

--- bark_mcts/models/behavior/tests/py_scenario_risk_function_test.py
+++ bark_mcts/models/behavior/tests/py_scenario_risk_function_test.py
@@ -104,6 +104,57 @@ class PyScenarioRiskFunctionTests(unittest.TestCase):
     self.assertAlmostEqual(scenario_risk_function.normalization_constant,
               desired_normalization_const, 5)
 
+  def test_1D_functions_complex(self):
+    param_server = ParameterServer()
+    prior_knowledge_region = PriorKnowledgeRegion({"1DDimensionName" : (-3.0, 7.0)})
+
+    def knowledge_function_1D_squared(region_boundaries):
+      if not len(region_boundaries) == 1:
+        raise ValueError("Only 1D knowledge function provided")
+      # template function is a*x^2+ b*x + c then indefinite integral is a/3.0*x^3 + b*x^2/2.0 + c*x
+      a1 = 10.0
+      b1 = 3.0
+      c1 = 2.0
+      indef_int = lambda x : a1/3.0*x**3 + b1*x**2/2.0 + c1*x
+      region_range = region_boundaries["1DDimensionName"]
+      if region_range[1] > 0 and region_range[0] < 0:
+        return abs(indef_int(0) - indef_int(region_range[0])) + \
+              abs(indef_int(region_range[1]) - indef_int(0))
+      else:
+        return abs(indef_int(region_range[1]) - indef_int(region_range[0]))
+
+    def scenario_risk_templ_func_1D_squared(region_boundaries):
+      if not len(region_boundaries) == 1:
+        raise ValueError("Only 1D scenario risk function provided")
+       # template function is a*x^2+ b*x + c then indefinite integral is a/3.0*x^3 + b*x^2/2.0 + c*x
+      a2 = -0.5
+      b2= 1.0
+      indef_int = lambda x : a2/2.0*x**2 + b2*x
+      region_range = region_boundaries["1DDimensionName"]
+      if region_range[1] > 0 and region_range[0] < 0:
+        return abs(indef_int(0) - indef_int(region_range[0])) + \
+              abs(indef_int(region_range[1]) - indef_int(0))
+      else:
+        return abs(indef_int(region_range[1]) - indef_int(region_range[0]))
+
+    prior_knowledge_function = PriorKnowledgeFunction(prior_knowledge_region, 
+                                                    knowledge_function_1D_squared,
+                                                    param_server)
+
+    scenario_risk_function = prior_knowledge_function.CalculateScenarioRiskFunction(
+                              scenario_risk_templ_func_1D_squared)
+
+    a1 = 10.0
+    b1 = 3.0
+    c1 = 2.0
+    a2 = -0.5
+    b2= 1.0
+    indef_int = lambda x : a1*a2*x**4/4 + a1*b2*x**3/3 + b1*a2*x**3/3 + b1*b2*x**2/2 + c1*a2*x**2/2 + c1*b2*x
+    area = abs(indef_int(0.0) - indef_int(-3.0)) + abs(indef_int(7.0)- indef_int(0.0))
+    desired_normalization_const = 1.0/(area)
+    self.assertAlmostEqual(scenario_risk_function.normalization_constant,
+              desired_normalization_const, 5)
+
 
 
 
