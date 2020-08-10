b48903ebf3db0473ca31f4ebc44c45029351c35a
juloberno <juloberno@gmail.com>
2020-08-10 21:45:17 +0200

    add cost const stat parameters

--- bark_mcts/models/behavior/param_config/mcts_parameters_from_param_server.hpp
+++ bark_mcts/models/behavior/param_config/mcts_parameters_from_param_server.hpp
@@ -28,13 +28,13 @@ inline mcts::MctsParameters MctsParametersFromParamServer(const commons::ParamsP
     parameters.random_heuristic.MAX_NUMBER_OF_ITERATIONS = params->GetInt("Mcts::RandomHeuristic::MaxNumIterations",
                                                          "Maximum number of environment steps performed by random heuristic", 1000);
 
-    parameters.uct_statistic.LOWER_BOUND = params->GetReal("Mcts::UctStatistic::ReturnLowerBound", "Lower return bound used for normalization in UCT Statistic", -1000);
-    parameters.uct_statistic.UPPER_BOUND = params->GetReal("Mcts::UctStatistic::ReturnUpperBound", "Upper return bound used for normalization in UCT Statistic", 100);
+    parameters.uct_statistic.LOWER_BOUND = params->GetReal("Mcts::ReturnLowerBound", "Lower return bound used for normalization in UCT Statistic", -1000);
+    parameters.uct_statistic.UPPER_BOUND = params->GetReal("Mcts::ReturnUpperBound", "Upper return bound used for normalization in UCT Statistic", 100);
     parameters.uct_statistic.EXPLORATION_CONSTANT = params->GetReal("Mcts::UctStatistic::ExplorationConstant", "Exploration constant of UCT", 0.7);
 
     parameters.hypothesis_statistic.COST_BASED_ACTION_SELECTION = params->GetBool("Mcts::HypothesisStatistic::CostBasedActionSelection", "True, if costs instead of rewards are used for action selection", false);
-    parameters.hypothesis_statistic.LOWER_COST_BOUND = params->GetReal("Mcts::HypothesisStatistic::LowerCostBound", "Upper cost bound used for normalization in UCT Statistic", 0);
-    parameters.hypothesis_statistic.UPPER_COST_BOUND = params->GetReal("Mcts::HypothesisStatistic::UpperCostBound", "Lower cost bound used for normalization in UCT Statistic", 1);
+    parameters.hypothesis_statistic.LOWER_COST_BOUND = params->GetReal("Mcts::LowerCostBound", "Upper cost bound used for normalization in UCT Statistic", 0);
+    parameters.hypothesis_statistic.UPPER_COST_BOUND = params->GetReal("Mcts::UpperCostBound", "Lower cost bound used for normalization in UCT Statistic", 1);
     parameters.hypothesis_statistic.PROGRESSIVE_WIDENING_HYPOTHESIS_BASED = params->GetBool("Mcts::HypothesisStatistic::ProgressiveWidening::HypothesisBased",
                                                          "True, if progressive widening is calculated separately for hypothesis", true);
     parameters.hypothesis_statistic.PROGRESSIVE_WIDENING_ALPHA = params->GetReal("Mcts::HypothesisStatistic::ProgressiveWidening::Alpha", "Alpha used for prog. widening", 0.25);
@@ -46,6 +46,18 @@ inline mcts::MctsParameters MctsParametersFromParamServer(const commons::ParamsP
     parameters.hypothesis_belief_tracker.PROBABILITY_DISCOUNT = params->GetReal("Mcts::BeliefTracker::ProbabilityDiscount", "Discount factor for probabilities", 0.7f);
     parameters.hypothesis_belief_tracker.POSTERIOR_TYPE = params->GetInt("Mcts::BeliefTracker::PosteriorType", "Zero for product, One for sum", 1);
     parameters.hypothesis_belief_tracker.FIXED_HYPOTHESIS_SET = fixed_hypothesis_set;
+
+    parameters.cost_constrained_statistic.LAMBDA = params->GetReal("Mcts::CostConstrainedStatistic::LambdaInit", "Initial lambda value", 2.0f);
+    parameters.cost_constrained_statistic.COST_UPPER_BOUND = parameters.hypothesis_statistic.UPPER_COST_BOUND
+    parameters.cost_constrained_statistic.COST_LOWER_BOUND = parameters.hypothesis_statistic.LOWER_COST_BOUND;
+    parameters.cost_constrained_statistic.REWARD_UPPER_BOUND = parameters.uct_statistic.UPPER_BOUND;
+    parameters.cost_constrained_statistic.REWARD_LOWER_BOUND = parameters.uct_statistic.LOWER_BOUND;
+    parameters.cost_constrained_statistic.COST_CONSTRAINT = 0.0f; // Set dynamically
+    parameters.cost_constrained_statistic.KAPPA = params->GetReal("Mcts::CostConstrainedStatistic::Kappa", "Exploration constant", 10.0f);
+    parameters.cost_constrained_statistic.GRADIENT_UPDATE_STEP = params->GetReal("Mcts::CostConstrainedStatistic::GradientUpdateScaling", "Update step scaling factor", 1.0f);
+    parameters.cost_constrained_statistic.TAU_GRADIENT_CLIP = params->GetReal("Mcts::CostConstrainedStatistic::TauGradientClip", "Values smaller than one increase allowed gradient", 1.0f);
+    parameters.cost_constrained_statistic.ACTION_FILTER_FACTOR = params->GetReal("Mcts::CostConstrainedStatistic::ActionFilterFactor",
+               "Scales node counts in relation to value differences, favoring less visited nodes", 0.5f);
 
     return parameters;
 }
